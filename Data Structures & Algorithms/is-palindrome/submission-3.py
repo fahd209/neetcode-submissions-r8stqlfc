87c067f1b6002aa4f12c2c762d875e24ff02505f
Fahd Saleh <166452601+fahd209@users.noreply.github.com>
2026-04-05 19:29:53 -0700

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,31 @@
+import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        # remove all the spaces
+        s = s.replace(" ", "").lower()
+
+        def is_alphanumeric(c):
+            return ord('a') <= ord(c) <= ord('z') or ord('0') <= ord(c) <= ord('9')
+            
+
+        # two pointers to check the left and right
+        L = 0
+        R = len(s) - 1
+
+        while L < R:
+            while L < R and not is_alphanumeric(s[L]):
+                L += 1
+            
+            while R > L and not is_alphanumeric(s[R]):
+                R -= 1
+
+            if s[L] != s[R]:
+                return False
+            L += 1
+            R -= 1
+
+
+        return True
+        
+        
