24869e1ccb1351f8ccd70bb3ec7f2defd74878d4
Fahd Saleh <166452601+fahd209@users.noreply.github.com>
2026-04-09 18:46:27 -0700

    Add: first-missing-positive - submission-3

---
+++ Data Structures & Algorithms/first-missing-positive/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        """
+        min: 3
+        [1,2,4]
+
+        [1,2,4]
+             i
+        """
+
+        numSet = set(nums)
+        max_num = max(nums)
+        min_positive = float('inf')
+
+        if max_num < 0:
+            return 1
+
+        for i in range(max_num + 2):
+            if i not in numSet:
+                if i != 0:
+                    min_positive = min(min_positive, i)
+
+        return min_positive
